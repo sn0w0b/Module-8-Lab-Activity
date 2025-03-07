bc24169913da90a87af1f121fc8e63aa2839670e
sn0w0b <167842670+sn0w0b@users.noreply.github.com>
2025-03-06 19:33:45 -0600

    Add files via upload

---
+++ Module_8___Problem_1.py
@@ -0,0 +1,12 @@
+# Martha Moreno Gonzalez
+# 03/06/2025
+
+# takes two inputs from a user and prints whether they are equal or not.
+def are_they_equal():
+    x = input("Enter your first value: ")
+    y = input("Enter your second value: ")
+    if x == y:
+        print("They are equal! :) ")
+    else:
+        print("They are not equal. :( ")
+are_they_equal()
